1de0b02c4d28c9a84201589858482b7ae4ab85d6
xwen <xwenx66@gmail.com>
2025-05-26 18:54:50 -0700

    added the project

---
+++ DreamInterpretation/DreamInterpretationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamInterpretationApp.swift
+//  DreamInterpretation
+//
+//  Created by Simon Xie on 2025/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DreamInterpretationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
